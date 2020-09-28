8fc069bc284b67e3da636761daba01fe1c5d18d7
Afrath49 <afrath4964@gmail.com>
2020-09-28 11:07:20 +0530

    initial commit

--- validate.js
+++ validate.js
@@ -1,15 +1,12 @@
-function validate() {
+function validate(){
+	var email = document.forms['frm']['email'];
 
- var email= document.forms['form']['email'];
-
- if (email.value="") {
- 	alert("enter email");
- }
-  if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value))
-  {
-    return (true);
-  }else if{
-    alert("You have entered an invalid email address!");
-    return (false);
-  }
+	if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)) {
+		alert('invalid email address');
+		email.focus();
+		return false;
+	}else{
+		alert('valid email address');
+		return true;
+	}
 }
